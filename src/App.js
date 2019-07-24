67e8e5be4216b7cb3d7737cc7e405c5ba242d454
Anila Qerimi <Gjogi jem>
2019-07-24 12:20:14 +0200

    Added pages,data.js, images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import "./App.css";
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingelRoom from './pages/SingelRoom';
+import Error from './pages/Error';
 
 function App() {
   return (
     <>
-    Hello Form App
+    <Home />
+    <Rooms />
+    <SingelRoom />
+    <Error />
     </>
   );
 }
---
+++ src/pages/SingelRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SingelRoom extends Component {
+    render() {
+        return(
+            <div>Hello prom SingelRoom page</div>
+        );
+    }
+}
+
+export default SingelRoom
